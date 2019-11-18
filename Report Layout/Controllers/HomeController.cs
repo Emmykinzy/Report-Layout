c5aef1295bd8b7bb053ad1a17473c0beedec6de4
Benjamin Caron-Lortie <32286421+ben-cl@users.noreply.github.com>
2019-11-17 21:36:27 -0500

    Controller get info from for to trip

--- Report Layout/Controllers/HomeController.cs
+++ Report Layout/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Report_Layout.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,8 +10,50 @@ namespace Report_Layout.Controllers
     public class HomeController : Controller
     {
         // GET: Home
-        public ActionResult Index()
+        public ActionResult Index(FormCollection collection)
         {
+            // new trip class object 
+            Trip trip = new Trip();
+
+            // Retrieve data from Form 
+
+            trip.TruckNumber = collection["truckNum"];
+
+            trip.DriverNumber = collection["driverNum"];
+
+            trip.CoDriverNumber = collection["co-driverNum"];
+
+            trip.TripNumber = collection["tripNum"];
+
+            trip.DateDeparted = Convert.ToDateTime(collection["dateDeparted"]);
+
+            trip.DateReturned = Convert.ToDateTime(collection["dateReturn"]);
+
+            trip.StateCode = collection["state"];
+
+            trip.MilesDriven = Convert.ToInt32(collection["milesDriven"]);
+
+            trip.ReceiptNumber = collection["receiptNum"];
+
+            trip.GallonsPurchased = Convert.ToDouble(collection["gallonsPurchased"]);
+
+            trip.TaxesPaid = Convert.ToDouble(collection["taxesPaid"]);
+
+            trip.StationName = collection["stationName"];
+
+            trip.Location = collection["stationLocation"];
+
+
+            // Manipulation 
+
+            // Return Report? 
+            //  ViewBag or Session? 
+
+            Session["trip"] = trip;
+
+            // retreieve the trip class object in report or elsewhere 
+
+
             return View();
         }
     }
--- Report Layout/Controllers/TripController.cs
+++ Report Layout/Controllers/TripController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Report_Layout.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,8 +10,11 @@ namespace Report_Layout.Controllers
     public class TripController : Controller
     {
         // GET: Trip
-        public ActionResult Index()
+        public ActionResult Index(FormCollection collection)
         {
+
+
+
             return View();
         }
     }
