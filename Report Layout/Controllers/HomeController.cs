b3221878c40d39f72a8f89e3c177b9d1db2d08e2
Benjamin Caron-Lortie <32286421+ben-cl@users.noreply.github.com>
2019-11-17 22:55:53 -0500

    Fix controller and add data successfuly to DB , Now we just need to better format the report

--- Report Layout/Controllers/HomeController.cs
+++ Report Layout/Controllers/HomeController.cs
@@ -10,49 +10,9 @@ namespace Report_Layout.Controllers
     public class HomeController : Controller
     {
         // GET: Home
-        public ActionResult Index(FormCollection collection)
+        public ActionResult Index()
         {
-            // new trip class object 
-            Trip trip = new Trip();
-
-            // Retrieve data from Form 
-
-            trip.TruckNumber = collection["truckNum"];
-
-            trip.DriverNumber = collection["driverNum"];
-
-            trip.CoDriverNumber = collection["co-driverNum"];
-
-            trip.TripNumber = collection["tripNum"];
-
-            trip.DateDeparted = Convert.ToDateTime(collection["dateDeparted"]);
-
-            trip.DateReturned = Convert.ToDateTime(collection["dateReturn"]);
-
-            trip.StateCode = collection["state"];
-
-            trip.MilesDriven = Convert.ToInt32(collection["milesDriven"]);
-
-            trip.ReceiptNumber = collection["receiptNum"];
-
-            trip.GallonsPurchased = Convert.ToDouble(collection["gallonsPurchased"]);
-
-            trip.TaxesPaid = Convert.ToDouble(collection["taxesPaid"]);
-
-            trip.StationName = collection["stationName"];
-
-            trip.Location = collection["stationLocation"];
-
-
-            // Manipulation 
-
-            // Return Report? 
-            //  ViewBag or Session? 
-
-            Session["trip"] = trip;
-
-            // retreieve the trip class object in report or elsewhere 
-
+            
 
             return View();
         }
--- Report Layout/Controllers/TripController.cs
+++ Report Layout/Controllers/TripController.cs
@@ -9,13 +9,65 @@ namespace Report_Layout.Controllers
 {
     public class TripController : Controller
     {
+        DBManager db = new DBManager();
+        public ActionResult Index()
+        {
+            //return RedirectToAction("Index", "Trip");
+            return View(); 
+        }
+
         // GET: Trip
+        [HttpPost]
         public ActionResult Index(FormCollection collection)
         {
+            // new trip class object 
+            Trip trip = new Trip();
+
+            // Retrieve data from Form 
+
+            trip.TruckNumber = collection["truckNum"];
+
+            trip.DriverNumber = collection["driverNum"];
+
+            trip.CoDriverNumber = collection["co-driverNum"];
+
+            trip.TripNumber = collection["tripNum"];
+
+            trip.DateDeparted = Convert.ToDateTime(collection["dateDeparted"]);
+
+            trip.DateReturned = Convert.ToDateTime(collection["dateReturn"]);
+
+            trip.StateCode = collection["state"];
+
+            String state = collection["state"];
+
+            trip.MilesDriven = Convert.ToInt32(collection["milesDriven"]);
+
+            trip.ReceiptNumber = collection["receiptNum"];
+
+            trip.GallonsPurchased = Convert.ToDouble(collection["gallonsPurchased"]);
+
+            trip.TaxesPaid = Convert.ToDouble(collection["taxesPaid"]);
+
+            trip.StationName = collection["stationName"];
+
+            trip.Location = collection["stationLocation"];
+
+
+            // Manipulation 
+
+            // Return Report? 
+            //  ViewBag or Session? 
+
+            Session["trip"] = trip;
+
+            db.Add(trip);
+            // retreieve the trip class object in report or elsewhere 
 
 
 
             return View();
+            //return RedirectToAction("Index", "Trip");
         }
     }
 }
